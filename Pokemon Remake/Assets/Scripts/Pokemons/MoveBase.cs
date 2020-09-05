de303909a877dd1489a17131d8f77d90c71c5a3c
Eichar46 <70171679+Eichar46@users.noreply.github.com>
2020-09-05 02:59:59 -0400

    Started to create moves for pokemon

---
+++ Pokemon Remake/Assets/Scripts/Pokemons/MoveBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Move", menuName = "Pokemon/Create new move")]
+
+public class MoveBase : ScriptableObject
+{
+    [SerializeField] string movename;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    [SerializeField] PokemonType type;
+
+    [SerializeField] int power;
+    [SerializeField] int accuracy;
+    [SerializeField] int pp;
+
+}
