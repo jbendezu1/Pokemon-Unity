d45fddbf2ab5260c496b02fe92de9f23052d09a8
Eichar46 <70171679+Eichar46@users.noreply.github.com>
2020-09-05 02:49:06 -0400

    Began adding Pokemon Objects, base stats calculator functions based on level, etc

---
+++ Pokemon Remake/Assets/Scripts/Pokemons/Pokemon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokemon
+{
+    PokemonBase statBase;
+    int level;
+
+    public Pokemon(PokemonBase pBase, int pLevel)
+    {
+        statBase = pBase;
+        level = pLevel;
+    }
+
+    public int Attack
+    {
+        get { return Mathf.FloorToInt((statBase.Attack * level) / 100f) + 5; }
+    }
+    public int Defense
+    {
+        get { return Mathf.FloorToInt((statBase.Defense * level) / 100f) + 5; }
+    }
+    public int SpecialAttack
+    {
+        get { return Mathf.FloorToInt((statBase.SpAttack * level) / 100f) + 5; }
+    }
+    public int SpecialDefense
+    {
+        get { return Mathf.FloorToInt((statBase.SpDefense * level) / 100f) + 5; }
+    }
+    public int Speed
+    {
+        get { return Mathf.FloorToInt((statBase.Speed * level) / 100f) + 5; }
+    }
+    public int HP
+    {
+        get { return Mathf.FloorToInt((statBase.MaxHP * level) / 100f) + 10; }
+    }
+    
+}
---
+++ Pokemon Remake/Assets/Scripts/Pokemons/PokemonBase.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create new Pokemon")]
+public class PokemonBase : ScriptableObject
+{
+    [SerializeField] string pokename;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+    [SerializeField] PokemonType type1;
+    [SerializeField] PokemonType type2;
+
+    //Base Stats for all pokemon
+    [SerializeField] int maxHP;
+    [SerializeField] int attack;
+    [SerializeField] int specialAttack;
+    [SerializeField] int defense;
+    [SerializeField] int specialDefense;
+    [SerializeField] int speed;
+
+
+
+    public string Name
+    {
+        get { return pokename; }
+    }
+
+    public string Description
+    {
+        get { return description; }
+    }
+    public Sprite FrontSprite
+    {
+        get { return frontSprite; }
+    }
+
+    public Sprite BackSprite
+    {
+        get { return backSprite; }
+    }
+    public int MaxHP
+    {
+        get { return maxHP; }
+    }
+
+    public int Attack
+    {
+        get { return attack; }
+    }
+    public int SpAttack
+    {
+        get { return specialAttack; }
+    }
+
+    public int Defense
+    {
+        get { return defense; }
+    }
+    public int SpDefense
+    {
+        get { return specialDefense; }
+    }
+
+    public int Speed
+    {
+        get { return speed; }
+    }
+
+    public PokemonType Type1
+    {
+        get { return type1; }
+    }
+
+    public PokemonType Type2
+    {
+        get { return type2; }
+    }
+
+}
+
+
+
+public enum PokemonType
+{
+    None,
+    Normal,
+    Grass,
+    Fire,
+    Water,
+    Flying,
+    Fighting,
+    Electric,
+    Rock,
+    Ground,
+    Ice,
+    Poison,
+    Psychic,
+    Dark,
+    Ghost,
+    Fairy,
+    Dragon,
+    Bug
+}
