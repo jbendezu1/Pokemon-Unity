ad42ba896ee09291cc9af57054fc0e1a51ca0826
jbendezu1 <jayden.bendezu@gmail.com>
2020-10-23 12:59:19 -0400

    Started Menu System and add Lapras
    Menu system exists only in Region 4

---
+++ Pokemon Remake/Assets/Scripts/Player/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject menu;
+    public Button firstButton;
+    public GameObject trainer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            toggleMenu();
+            if (menu.activeSelf)
+                firstButton.Select();
+        }
+    }
+
+    void toggleMenu()
+    {
+        if (menu.activeSelf)
+            menu.SetActive(false);
+        else
+            menu.SetActive(true);
+        
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        DontDestroyOnLoad(trainer);
+    }
+
+
+}
--- Pokemon Remake/Assets/Scripts/Player/Player.cs
+++ Pokemon Remake/Assets/Scripts/Player/Player.cs
@@ -8,83 +8,87 @@ using Vector2 = UnityEngine.Vector2;
 public class Player : MonoBehaviour
 {
     private string spriteName;
-	public float moveSpeed;
+    public float moveSpeed;
+    private GameObject menu;
+
 
     public LayerMask Ocean;
     public LayerMask Entrance;
     public LayerMask Shore;
-	public LayerMask Foreground;
-	public LayerMask Grass;
+    public LayerMask Foreground;
+    public LayerMask Grass;
 
-	private bool isMoving;
+    private bool isMoving;
     private bool onMilotic;
-	private Vector2 input;
+    private Vector2 input;
 
-	private Animator animator;
+    private Animator animator;
     private SpriteRenderer spriterenderer;
 
-	private void Awake()
-	{
-		animator = GetComponent<Animator>();
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
         spriterenderer = GetComponent<SpriteRenderer>();
+        menu = GameObject.Find("Menu");
     }
 
     private void Update()
-	{
-		if (!isMoving)
-		{
-			input.x = Input.GetAxisRaw("Horizontal");
-			input.y = Input.GetAxisRaw("Vertical");
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
 
             // If player is going left/right lock up/down movement
-			if (input.x != 0) input.y = 0;
+            if (input.x != 0) input.y = 0;
 
             // Create player movement
-			if (input != Vector2.zero)
-			{
-				animator.SetFloat("MoveX", input.x);
-				animator.SetFloat("MoveY", input.y);
+            if (input != Vector2.zero && !menu.activeSelf)
+            {
+                animator.SetFloat("MoveX", input.x);
+                animator.SetFloat("MoveY", input.y);
 
-				var targetPosition = transform.position;
-				targetPosition.x += input.x;
-				targetPosition.y += input.y;
-				if (isWalkable(targetPosition))
-					StartCoroutine(Move(targetPosition));
-			}
-		}
-		animator.SetBool("isMoving", isMoving);
-	}
+                var targetPosition = transform.position;
+                targetPosition.x += input.x;
+                targetPosition.y += input.y;
+                if (isWalkable(targetPosition))
+                    StartCoroutine(Move(targetPosition));
+            }
+        }
+        animator.SetBool("isMoving", isMoving);
 
-	IEnumerator Move(Vector3 targetPosition)
-	{
-		isMoving = true;
+    }
 
-		while ((targetPosition - transform.position).sqrMagnitude > Mathf.Epsilon)
-		{
-			transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-			yield return null;
-		}
-		transform.position = targetPosition;
-		isMoving = false;
+    IEnumerator Move(Vector3 targetPosition)
+    {
+        isMoving = true;
 
-		checkForEncounter();
-	}
+        while ((targetPosition - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPosition;
+        isMoving = false;
+
+        checkForEncounter();
+    }
 
     // Prevent player from moving over foreground tiles
-	private bool isWalkable(Vector3 targetposition){
+    private bool isWalkable(Vector3 targetposition) {
 
-		if (Physics2D.OverlapCircle(targetposition, 0.1f, Foreground) != null){
-			return false;
-		}
+        if (Physics2D.OverlapCircle(targetposition, 0.1f, Foreground) != null) {
+            return false;
+        }
 
-        if (Physics2D.OverlapCircle(targetposition,0.1f, Ocean) != null && spriterenderer.sprite.name.Contains("Trainer"))
+        if (Physics2D.OverlapCircle(targetposition, 0.1f, Ocean) != null && spriterenderer.sprite.name.Contains("Trainer"))
         {
             string spriteName = spriterenderer.sprite.name;
             Debug.Log(spriteName);
             return false;
         }
         return true;
-	}
+    }
 
     private void checkForEncounter()
     {
@@ -96,4 +100,5 @@ public class Player : MonoBehaviour
             }
         }
     }
+
 }
