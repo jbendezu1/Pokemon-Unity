a4348182a4dd8dcf80f917cb558c1a186fe67c30
Eichar46 <70171679+Eichar46@users.noreply.github.com>
2020-12-06 21:07:49 -0500

    Intro update

--- Pokemon Remake/Assets/Scripts/Intro/Intro.cs
+++ Pokemon Remake/Assets/Scripts/Intro/Intro.cs
@@ -14,8 +14,8 @@ public class Intro : MonoBehaviour
     [SerializeField] Image BG;
     [SerializeField] Image BG2;
     [SerializeField] Image BG3;
-    [SerializeField] Image BG0;
     [SerializeField] Text dialogText;
+    [SerializeField] GameObject inputField;
     [SerializeField] int lettersPerSecond;
 
 
@@ -38,23 +38,35 @@ public class Intro : MonoBehaviour
         BG.gameObject.SetActive(true);
         BG2.gameObject.SetActive(true);
         BG3.gameObject.SetActive(true);
-        BG0.gameObject.SetActive(true);
         IntroBackground.gameObject.SetActive(false);
+        inputField.SetActive(false);
         yield return StartCoroutine(RunIntro());
     }
 
     public IEnumerator RunIntro()
     {
         var sequence = DOTween.Sequence();
-        yield return (BG0.DOFade(0f, 5f));
-        yield return sequence.Join(DeveloperText.DOFade(0f, 2f)).SetDelay(2);
-        yield return sequence.Join(DeveloperImage.DOFade(0f, 2f)).SetDelay(2);
+        new WaitForSeconds(2f);
+        yield return sequence.Join(DeveloperText.DOFade(0f, 2f));
+        yield return sequence.Join(DeveloperImage.DOFade(0f, 2f));
         IntroLogo.gameObject.SetActive(true);
         PushToStart.gameObject.SetActive(true);
         yield return (BG.DOFade(0f, 5f).SetDelay(2));
+        new WaitForSeconds(1.5f);
         int count = 0;
+        int changeColor = 0;
         while (count == 0)
         {
+            if (changeColor % 2 == 0)
+            {
+                yield return sequence.Append(PushToStart.DOColor(Color.black, 0.1f));
+                changeColor++;
+            }
+            if (changeColor % 2 == 1)
+            {
+                yield return sequence.Append(PushToStart.DOColor(Color.black, 0.1f));
+                changeColor++;
+            }
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 count++;
@@ -63,10 +75,13 @@ public class Intro : MonoBehaviour
             }
         }
         IntroBackground.gameObject.SetActive(true);
-        yield return sequence.Append(BG2.DOFade(0f, 5f)).SetDelay(2);
-        StartCoroutine (TypeDialog(""));
-        StartCoroutine (TypeDialog("Hello there ummm kid and welcome to the world of Pokemon. I'm terribly sorry for what happened, your father was a great man and an excellent trainer.\n" +
-            "I'm sure now you must want to set off on yourown adventure and continue your fathers legacy as one of the elite trainers here in the Noraldo region"));
+        yield return (BG2.DOFade(0f, 5f)).SetDelay(2);
+        new WaitForSeconds(2f);
+        yield return StartCoroutine(TypeDialog(""));
+        yield return StartCoroutine(TypeDialog("Hello there and welcome to the world of Pokemon. I'm terribly sorry for what happened, your father was a great man and an excellent trainer."));
+        yield return StartCoroutine(TypeDialog("Who am I? Why I'm Professor Carbone of course, leading expert in Pokemon and programming. I'm also the person in charge to hand you your partner pokemon."));
+        yield return StartCoroutine(TypeDialog("I'm sure now you must want to set off on your own adventure and continue your fathers legacy as one of the elite trainers here in the Noraldo region."));
+        yield return StartCoroutine(TypeDialog("Now ummm remind me again, what was your name?"));
     }
     public IEnumerator TypeDialog(string dialog)
     {
@@ -78,6 +93,10 @@ public class Intro : MonoBehaviour
         }
 
         yield return new WaitForSeconds(1f);
+    }
+    public IEnumerator GetPlayerName()
+    {
+        yield return null;
     }
     public void ConnectRegion()
     {
