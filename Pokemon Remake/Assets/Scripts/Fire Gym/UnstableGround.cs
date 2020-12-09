357b607bee9f16b0ec7e5af63583f9df1c860dae
jbendezu1 <jayden.bendezu@gmail.com>
2020-12-08 20:55:13 -0500

    Started fire gym challenge

--- Pokemon Remake/Assets/Scripts/Fire Gym/UnstableGround.cs
+++ Pokemon Remake/Assets/Scripts/Fire Gym/UnstableGround.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class UnstableGround : MonoBehaviour
 {
+    Vector2 startingPos;
+    private SpriteRenderer thisSprite;
+    private float speed = 100f;
+    public float amount = 0.01f;
+    private float y;
+    bool willFall = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        y = transform.position.y;
+        thisSprite = this.GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // If the sprite is fine, randomly see if it will fall
+        if (thisSprite.name != null && !willFall)
+        {
+            float rando = Random.Range(0, 10);
+
+            if (rando == 1)
+            {
+                Shakey();
+            }
+        }
+
+        if (thisSprite.sprite == null)
+        {
+            thisSprite.sprite = ;
+        }
+    }
+
+
+
+    void Shakey()
+    {
+        y = startingPos.y + Mathf.Sin(Time.time * speed) * amount;
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        willFall = true;
     }
 }
