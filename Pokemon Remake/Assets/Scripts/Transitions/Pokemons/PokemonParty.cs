85682e2c89ca49265502a32c3cb618197127de53
jbendezu1 <jayden.bendezu@gmail.com>
2020-12-09 17:28:34 -0500

    Fixed moves and such

---
+++ Pokemon Remake/Assets/Scripts/Transitions/Pokemons/PokemonParty.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PokemonParty : MonoBehaviour
+{
+    [SerializeField] List<Pokemon> pokemons;
+
+    public List<Pokemon> Pokemons
+    {
+        get
+        {
+            return pokemons;
+        }
+    }
+
+    private void Start()
+    {
+       foreach (var pokemon in pokemons)
+        {
+            pokemon.Init();
+        }
+    }
+    public Pokemon GetHealthyPokemon()
+    {
+        return pokemons.Where(x => x.HP > 0).FirstOrDefault();
+    }
+
+    public void Update()
+    {
+        if (this.gameObject.CompareTag("Player"))
+        {
+            foreach (Pokemon x in pokemons)
+            {
+                if (x.HP <= 0)
+                {
+                    pokemons.Remove(x);
+                }
+            }
+        }
+        
+    }
+}
