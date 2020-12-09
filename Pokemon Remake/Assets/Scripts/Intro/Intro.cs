f90dd6a997cf489c98ca2d4bbdfac39b62a60f3f
Eichar46 <70171679+Eichar46@users.noreply.github.com>
2020-12-08 22:07:06 -0500

    Intro finished

--- Pokemon Remake/Assets/Scripts/Intro/Intro.cs
+++ Pokemon Remake/Assets/Scripts/Intro/Intro.cs
@@ -19,16 +19,14 @@ public class Intro : MonoBehaviour
     [SerializeField] Image BG2;
     [SerializeField] Image BG3;
     [SerializeField] Image BG4;
-    [SerializeField] Image StarterSelectionScreen;
     [SerializeField] Text dialogText;
     [SerializeField] GameObject inputField;
     [SerializeField] InputField inputFieldText;
     [SerializeField] int lettersPerSecond;
-    [SerializeField] Color highlightedColor;
-    [SerializeField] BattleDialogBox dialogBox;
-    PartyMemberUI[] memberSlots;
-    public List<Pokemon> pokemons;
-    [SerializeField] List<Text> actionTexts;
+
+
+
+    PokemonParty playerParty;
 
 
     Color DevIm;
@@ -36,8 +34,7 @@ public class Intro : MonoBehaviour
     Color DevT;
     Color PTS;
     string playerName = "";
-    Pokemon _pokemon;
-    int currentMember;
+    
 
     public IEnumerator Start()
     {
@@ -49,7 +46,6 @@ public class Intro : MonoBehaviour
         BG2.gameObject.SetActive(true);
         BG3.gameObject.SetActive(true);
         BG4.gameObject.SetActive(true);
-        StarterSelectionScreen.gameObject.SetActive(true);
         IntroBackground.gameObject.SetActive(true);
         yield return StartCoroutine(BeforeIntro());
         yield return StartCoroutine(RunIntro());
@@ -86,15 +82,20 @@ public class Intro : MonoBehaviour
         yield return new WaitForSeconds(2);
         yield return StartCoroutine(TypeDialog(""));
         yield return StartCoroutine(TypeDialog("Hello there and welcome to the world of Pokemon. I'm terribly sorry for what happened, your father was a great man and an excellent trainer."));
-        yield return StartCoroutine(TypeDialog("Who am I? Why I'm Professor Carbone of course, leading expert in Pokemon and programming. I'm also the person in charge to hand you your partner pokemon."));
+        yield return StartCoroutine(TypeDialog("Who am I? Why I'm Professor Carbone of course, leading expert in Pokemon and programming."));
         yield return StartCoroutine(TypeDialog("I'm sure now you must want to set off on your own adventure and continue your fathers legacy as one of the elite trainers here in the Noraldo region."));
-        yield return StartCoroutine(TypeDialog("Now "+playerName + ", why don't we get you your partner Pokemon?"));
+        yield return StartCoroutine(TypeDialog("Here in the Noraldo Region there are a few things that you don't normally see in other regions around the world.\n" +
+            "First of all, Pokemon create a huge bond with you, but if you let them faint, they won't trust you anymore and leave."));
+        yield return StartCoroutine(TypeDialog("Second, the only item available to you are Noraldo balls. You may not use any other items here as they are banned."));
+        yield return StartCoroutine(TypeDialog("Third, you may only ever have six pokemon with you. We are poor and there are no PC systems... So sad..."));
+        yield return StartCoroutine(TypeDialog("Finally, use the arrow keys to move around, Enter key is used for selections, Z key is used for interacting with others\n" +
+            "and M key is used to access your Menu. Good luck on your journey" + playerName));
         yield return sequence.Append(IntroBackground.DOFade(0f, 2f));
         yield return sequence.Join(Professor.DOFade(0f,2f));
         yield return sequence.Join(DialogBox.DOFade(0f, 2f));
         yield return (BG4.DOFade(0f, 5f)).SetDelay(1);
-        StarterSelection();
-        
+        //StarterSelection();
+        ConnectRegion();
     }
     public IEnumerator TypeDialog(string dialog)
     {
@@ -107,56 +108,14 @@ public class Intro : MonoBehaviour
 
         yield return new WaitForSeconds(1f);
     }
-    public void StarterSelection()
-    {
-        HandlePartySelection();
-        ConnectRegion();
-    }
+    
     public string GetPlayerName()
     {
         playerName = inputFieldText.text;
         return playerName;
     }
     
-    void HandlePartySelection()
-    {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-            ++currentMember;
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-            --currentMember;
-        currentMember = Mathf.Clamp(currentMember, 0, 2);
-
-        dialogBox.UpdateActionSelection(currentMember);
-
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            if (currentMember == 0)
-            {
-
-            }
-            if (currentMember == 1)
-            {
-
-            }
-            if (currentMember == 2)
-            {
-
-            }
-
-        }
-    }
-    public void UpdateActionSelection(int selectedAction)
-    {
-        for (int i = 0; i < actionTexts.Count; i++)
-        {
-            if (i == selectedAction)
-            {
-                actionTexts[i].color = highlightedColor;
-            }
-            else
-                actionTexts[i].color = Color.black;
-        }
-    }
+   
     public void ConnectRegion()
     {
         SceneManager.LoadScene("Region 1");
